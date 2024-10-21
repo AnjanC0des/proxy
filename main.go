a686357bd8ec91fcaf87375e1c44239a0be8d4dc
anjanc0des <129589930+AnjanC0des@users.noreply.github.com>
2024-10-21 15:36:35 +0000

    some dns changes

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/elazarl/goproxy"
@@ -11,7 +14,19 @@ import (
 func main() {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
-
+	proxy.Tr.Proxy = func(req *http.Request) (*url.URL, error) {
+		return nil, nil
+	}
+	proxy.Tr.DialContext = (&net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+		Resolver: &net.Resolver{
+			PreferGo: true,
+			Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+				return net.Dial(network, "8.8.8.8:53") // Using Google's DNS server
+			},
+		},
+	}).DialContext
 	// Block YouTube and Instagram during specific hours
 	proxy.OnRequest(goproxy.DstHostIs(
 		"www.youtube.com",
