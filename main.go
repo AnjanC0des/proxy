31a9b3697003e7939a6d686433f1a3ac6c3baa04
anjanc0des <129589930+AnjanC0des@users.noreply.github.com>
2024-10-21 15:02:09 +0000

    init commit

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/elazarl/goproxy"
+)
 
 func main() {
-	fmt.Println("Hello, Go in CodeSandbox!")
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = true
+
+	// Block YouTube and Instagram during specific hours
+	proxy.OnRequest(goproxy.DstHostIs(
+		"www.youtube.com",
+		"www.instagram.com")).DoFunc(
+		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			currentHour := time.Now().Hour()
+			if currentHour >= 9 && currentHour <= 23 { // Block from 9 AM to 5 PM
+				return r, goproxy.NewResponse(r, goproxy.ContentTypeText, http.StatusForbidden, "Access to this site is blocked during working hours.")
+			}
+			return r, nil
+		})
+
+	log.Fatal(http.ListenAndServe(":8080", proxy))
 }
